7cc85a4b139e10fe7991a2e319df6380ca453ebf
CarriereF <flo_fun31photmail.fr>
2015-12-14 13:36:04 +0100

    ajout index.php

---
+++ Applis_E4/application_gsbMVC/index.php
@@ -0,0 +1,34 @@
+<?php
+//test
+require_once("include/fct.inc.php");
+require_once ("include/class.pdogsb.inc.php");
+include("vues/v_entete.php") ;
+session_start();
+$pdo = PdoGsb::getPdoGsb();
+$estConnecte = estConnecte();
+$estConnecte_comptable = estConnecte_comptable();
+if(!isset($_REQUEST['uc']) && !$estConnecte && !$estConnecte_comptable){
+     $_REQUEST['uc'] = 'connexion';
+}	 
+
+else if (isset($_SESSION['idVisiteur'])) {
+	$_REQUEST['uc'] = 'etatFrais';
+}
+else {
+	$_REQUEST['uc'] = 'gererFrais';
+}
+$uc = $_REQUEST['uc'];
+switch($uc){
+	case 'connexion':{
+		include("controleurs/c_connexion.php");break;
+	}
+	case 'gererFrais' :{
+		include("controleurs/c_gererFrais.php");break;
+	}
+	case 'etatFrais' :{
+		include("controleurs/c_etatFrais.php");break; 
+	}
+}
+include("vues/v_pied.php") ;
+?>
+
