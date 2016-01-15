a771da1b37575614ca6108a25a8e1461c12d3131
silly-kid <flo_fun31@hotmail.fr>
2016-01-15 14:26:02 +0100

    Nouveau projet_test

---
+++ projet_test2/GSB_AppliMVC/gsbMVC/controleurs/c_connexion.php
@@ -0,0 +1,44 @@
+﻿<?php
+if(!isset($_REQUEST['action'])){
+	$_REQUEST['action'] = 'demandeConnexion';
+}
+$action = $_REQUEST['action'];
+switch($action){
+	case 'demandeConnexion':{
+		include("vues/v_connexion.php");
+		break;
+	}
+	case 'valideConnexion':{
+		$login = $_REQUEST['login'];
+		$mdp = $_REQUEST['mdp'];
+		$visiteur = $pdo->getInfosVisiteur($login,$mdp);
+		$comptable = $pdo->getInfosComptable($login,$mdp);// nouvelle fonction 
+		//condition modifié
+		if(!is_array( $visiteur)&&(!is_array( $comptable))){
+			ajouterErreur("Login ou mot de passe incorrect");
+			include("vues/v_erreurs.php");
+			include("vues/v_connexion.php");
+		}
+		else if(is_array( $visiteur)){
+			$id = $visiteur['id'];
+			$nom =  $visiteur['nom'];
+			$prenom = $visiteur['prenom'];
+			connecter($id,$nom,$prenom);
+			include("vues/v_sommaire.php");
+		}
+		//condition rajouté
+		else if(is_array( $comptable)){
+			$id = $comptable['id'];
+			$nom =  $comptable['nom'];
+			$prenom = $comptable['prenom'];
+			connecter($id,$nom,$prenom);
+			include("vues/v_sommaire.php");
+		}
+		break;
+	}
+	default :{
+		include("vues/v_connexion.php");
+		break;
+	}
+}
+?>
